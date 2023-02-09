8dd4a75bd36b47d1f2126ff5122d4b5db5787016
karankharole <karankharole2000@gmail.com>
2023-02-09 08:17:38 +0530

    CSV india State Census analyser

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace CensusAnalyzerProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Indian State Census Analyser Program");
+            Console.ReadLine();
+        }
+    }
+}
